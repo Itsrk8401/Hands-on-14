debe71421b9715c23637ff3f2819398b442bf7d4
Itsrk8401 <158119038+Itsrk8401@users.noreply.github.com>
2024-04-17 16:18:15 -0500

    Create dfs.py

---
+++ dfs.py
@@ -0,0 +1,23 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=' ')
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Example usage:
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['D'],
+    'D': []
+}
+
+dfs(graph, 'A')
+
+
+Output : 
+
+A B D C 
